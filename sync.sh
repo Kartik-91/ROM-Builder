66186705ff6a682226009153911b36a93b787391
Kartik Banwala <KartikbanwalaOP@gmail.com>
2022-09-11 18:22:41 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -16,8 +16,10 @@ repo sync -j$(nproc --all) --force-sync --no-tags --no-clone-bundle
 
 # Clone Trees
 git clone --single-branch --depth=1 $DT_LINK $DT_PATH || { echo "ERROR: Failed to Clone the Device Trees!" && exit 1; }
-wget https://siddrive.ga/0:/vendor.img device/xiaomi/pissarro/prebuilts
-wget https://siddrive.ga/0:/odm.img device/xiaomi/pissarro/prebuilts
+cd device/xiaomi/pissarro/prebuilts/
+wget https://siddrive.ga/0:/vendor.img
+wget https://siddrive.ga/0:/odm.img
+cd ../../../..
 git clone --single-branch --depth=1 $DCT_LINK $DCT_PATH
 git clone --single-branch --depth=1 $VT_LINK $VT_PATH
 git clone --single-branch --depth=1 $VCT_LINK $VCT_PATH
